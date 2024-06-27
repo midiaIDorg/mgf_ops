247980f9e37387eeb05c97455ad4c604aae3c388
matteo <matteo.lacki@gmail.com>
2024-06-27 10:47:06 +0200

    have a general tool to adjust headers. can also be used to change one line into another one.

--- tools/mgf_change_headers.py
+++ tools/mgf_change_headers.py
@@ -1,43 +1,56 @@
+#!/usr/bin/env python
+import argparse
 import re
 import sys
 
-if len(sys.argv) != 3:
-    print("Call me maybe with:")
-    print("python change_headers.py input_mgf output_mgf")
-else:
-    _, in_mgf, out_mgf = sys.argv
+# sage_format = r"TITLE=_\.(?P<clusterID_0>[^\.]+)\.(?P<clusterID_1>[^\.]+)\.2 File:\"_\.d\", NativeID:\"merged=(?P<merged>[^ ]+) frame=(?P<frame>[^ ]+) scanStart=(?P<scanStart>[^ ]+) scanEnd=(?P<scanEnd>[^ ]+) IonMobility:(?P<IonMobility>[^ ]+) intensity=(?P<intensity>\w+)"
 
-    # adjust htis if not working
-    sage_title = r'TITLE=_.([^\.]+)\.([^\.]+)\.2 File:"_.d", NativeID:"merged=([^ ]+) frame=([^ ]+) scanStart=([^ ]+) scanEnd=([^ ]+)", IonMobility:"([^,]+), intensity=([^"]+)"'
+# fragpipe_format = 'TITLE=test.{clusterID_0}.{clusterID_1}.2 File:"test.d", NativeID:"merged={merged} frame={frame} scanStart={scanStart} scanEnd={scanEnd}", IonMobility:"{IonMobility}", intensity:{intensity}\n'
 
-    fragpipe_title = 'TITLE=test.{}.{}.2 File:"test.d", NativeID:"merged={} frame={} scanStart={} scanEnd={}", IonMobility:"{}"\n'
+# args = {
+#     "in_mgf": "/home/matteo/Poligono/test.mgf",
+#     "out_mgf": None,
+#     "in_header_format": sage_format,
+#     "out_header_format": fragpipe_format,
+#     "fast_check": "TITLE",
+# }
 
-    sage_title_pattern = re.compile(sage_title)
 
-    # in_mgf = "second_gen_sage.mgf"
-    # out_mgf = "/tmp/fragpipe.mgf"
+parser = argparse.ArgumentParser(
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    description="Change one flavor of MGF headers into another.",
+)
+parser.add_argument(
+    "in_mgf",
+    help="Input MGF",
+)
+parser.add_argument(
+    "--in_header_format",
+    help="Header format to match to. Must use named regex groups, `?P<name>` thingies. Defaults to a flavor of midiaID SAGE.",
+    default=r"TITLE=_\.(?P<clusterID_0>[^\.]+)\.(?P<clusterID_1>[^\.]+)\.2 File:\"_\.d\", NativeID:\"merged=(?P<merged>[^ ]+) frame=(?P<frame>[^ ]+) scanStart=(?P<scanStart>[^ ]+) scanEnd=(?P<scanEnd>[^ ]+) IonMobility:(?P<IonMobility>[^ ]+) intensity=(?P<intensity>\w+)",
+    type=str,
+)
+parser.add_argument(
+    "--out_header_format",
+    help="Header format to match to. Must use a subset of names of groups specified in `in_header_format`. Defaults to a flavor of Fragpipe.",
+    default='TITLE=test.{clusterID_0}.{clusterID_1}.2 File:"test.d", NativeID:"merged={merged} frame={frame} scanStart={scanStart} scanEnd={scanEnd}", IonMobility:"{IonMobility}", intensity:{intensity}\n',
+    type=str,
+)
+parser.add_argument(
+    "--fast_check",
+    help="A faster than regex check if the line should qualify for regex match.",
+    default="TITLE",
+    type=str,
+)
+args = parser.parse_args().__dict__
 
-    with open(in_mgf, "r") as _in, open(out_mgf, "w") as _out:
+
+if __name__ == "__main__":
+    in_header_pattern = re.compile(args["in_header_format"])
+    with open(args["in_mgf"], "r") as _in:
         for line in _in:
-            match = sage_title_pattern.search(line)
-            if match:
-                (
-                    clusterID,
-                    clusterID,
-                    floor_frame_wmean,
-                    floor_frame_wmean,
-                    scan_min,
-                    scan_max,
-                    ion_mobility_wmean,
-                    intensity,
-                ) = match.groups()
-                line = fragpipe_title.format(
-                    clusterID,
-                    clusterID,
-                    floor_frame_wmean,
-                    floor_frame_wmean,
-                    scan_min,
-                    scan_max,
-                    ion_mobility_wmean,
-                )
-            _out.write(line)
+            if len(args["fast_check"]) == 0 or line[:5] == args["fast_check"]:
+                match = in_header_pattern.search(line)
+                if match:
+                    line = args["out_header_format"].format(**match.groupdict())
+            print(line, end="")
