a47747189c45f2b9a212fd3801fc44b69ca67ee3
Mateusz Lacki <matteo.lacki@gmail.com>
2025-10-28 13:00:04 +0100

    porting to Rust

--- __dev/timstofu_compatibility.py
+++ __dev/timstofu_compatibility.py
@@ -22,34 +22,34 @@ locals().update(
 )
 
 
-# spectrum_direct = get_direct_spectrum(
-#     spec_id, config.fragments.mz_digits, pseudomsms, headers, config.endions
-# )
-# with open(f"/tmp/spec_{spec_id}.mgf", "w") as f:
-#     f.write(spectrum_direct)
+spectrum_direct = get_direct_spectrum(
+    spec_id, config.fragments.mz_digits, pseudomsms, headers, config.endions
+)
+with open(f"/tmp/spec_{spec_id}.mgf", "w") as f:
+    f.write(spectrum_direct)
 
-# pprint(spectrum_direct)
-# len(spectrum_direct)
+pprint(spectrum_direct)
+len(spectrum_direct)
 
-# spec_id = 10253
+spec_id = 10253
 
-# buggy_spec = ascii2str(mgf[spectrum_idx[spec_id] : spectrum_idx[spec_id + 1]])
-# with open(f"/tmp/buggy_spec_{spec_id}.mgf", "w") as f:
-#     f.write(buggy_spec)
+buggy_spec = ascii2str(mgf[spectrum_idx[spec_id] : spectrum_idx[spec_id + 1]])
+with open(f"/tmp/buggy_spec_{spec_id}.mgf", "w") as f:
+    f.write(buggy_spec)
 
 
-# would be comfy to have something like a final idx
-# precursor_to_frag_idx = pseudomsms.precursors.fragment_spectrum_start.to_numpy()
-# precursor_to_frag_cnt = pseudomsms.precursors.fragment_event_cnt.to_numpy()
-# headers_idx = headers.idx
-# headers_ascii = headers.ascii
-# fragment_mz_digits = config.fragments.mz_digits
-# mzs = pseudomsms.fragments.mz
-# int_mz_to_hash = MZ.int_mz_to_hash
-# mz_hash_to_ascii = MZ.hash_to_ascii_idx
-# mz_ascii = MZ.ascii
-# intensities = pseudomsms.fragments.intensity
-# intensity_to_hash = INTENSITY.intensity_to_hash
-# intensity_hash_to_ascii = INTENSITY.hash_to_ascii_idx
-# intensity_ascii = INTENSITY.ascii
-# prec_idx = 0
+would be comfy to have something like a final idx
+precursor_to_frag_idx = pseudomsms.precursors.fragment_spectrum_start.to_numpy()
+precursor_to_frag_cnt = pseudomsms.precursors.fragment_event_cnt.to_numpy()
+headers_idx = headers.idx
+headers_ascii = headers.ascii
+fragment_mz_digits = config.fragments.mz_digits
+mzs = pseudomsms.fragments.mz
+int_mz_to_hash = MZ.int_mz_to_hash
+mz_hash_to_ascii = MZ.hash_to_ascii_idx
+mz_ascii = MZ.ascii
+intensities = pseudomsms.fragments.intensity
+intensity_to_hash = INTENSITY.intensity_to_hash
+intensity_hash_to_ascii = INTENSITY.hash_to_ascii_idx
+intensity_ascii = INTENSITY.ascii
+prec_idx = 0
